d8c1ffa1c057df08a3031b4a055c857b0ea22da5
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-08-21 20:07:32 -0500

    add kbd-range

--- data.js
+++ data.js
@@ -1,6 +1,13 @@
 module.exports = {
     data: [
         {
+            command: '!kbd-range',
+            path: './assets/kbd-range.png',
+            title: "Go get that vissy!",
+            height: 462,
+            width: 755
+        },
+        {
             command: '!zulrah-void',
             path: './assets/zulrah-void.png',
             title: "So, you think you're ready to take on Zulrah?",
