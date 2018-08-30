7d976c0eedc47db2683f9e1760a595343f83184e
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-08-30 10:00:49 -0500

    added several

--- data.js
+++ data.js
@@ -1,6 +1,13 @@
 module.exports = {
     data: [
         {
+            command: '!arma',
+            path: './assets/arma.png',
+            title: "GL Bird Hunting!",
+            height: 462,
+            width: 755
+        },
+        {
             command: '!kbd-range',
             path: './assets/kbd-range.png',
             title: "Go get that vissy!",
@@ -15,9 +22,37 @@ module.exports = {
             width: 775
         },
         {
+            command: '!raids-solo',
+            path: './assets/raids-solo.png',
+            title: "Alright, Woox, go show us how it's done!",
+            height: 464,
+            width: 756
+        },
+        {
             command: '!raids-standard',
             path: './assets/raids-standard.png',
             title: "Hope you don't get dragon thrown-axes!",
+            height: 464,
+            width: 756
+        },
+        {
+            command: '!sara-duo',
+            path: './assets/sara-duo.png',
+            title: "",
+            height: 464,
+            width: 756
+        },
+        {
+            command: '!sara-solo',
+            path: './assets/sara-solo.png',
+            title: "",
+            height: 464,
+            width: 756
+        },
+        {
+            command: '!vetion',
+            path: './assets/vetion.png',
+            title: "Giant spiders? Count me out.",
             height: 464,
             width: 756
         },
