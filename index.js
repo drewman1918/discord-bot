8b199901b4d4fb93e8ad53823174ed0e460d35b7
Drew Bloomfield <DrewBloomfield@drews-air.home>
2018-10-11 11:33:11 -0500

    fix formula for tanksplit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log('How may I serve you?')
-    client.user.setGame('Try !setups')
+    client.user.setActivity('Try !setups')
 })
 
 const addPicture = async (attachmentPath, attachmentName, attachmentTitle, message, width, height) => {
@@ -34,32 +34,25 @@ client.on('message', message => {
     } else if (message.content.startsWith('!tanksplit')) {
         let command = message.content.replace(/\s/g, '');
         
-        let numbers = command.split('!tanksplit')[1].trim()
+        let numbers = command.split('!tanksplit')[1].trim().toLowerCase()
 
         let amount = numbers.split('/')[0]
         let n = numbers.split('/')[1]
         
         if (amount.includes('k')) {
             amount = Number(amount.split('').filter(letter => letter !== 'k').join('')) * 1000
-        } else if (amount.includes('K')) {
-            amount = Number(amount.split('').filter(letter => letter !== 'K').join('')) * 1000
         } else if (amount.includes('m')) {
             amount = Number(amount.split('').filter(letter => letter !== 'm').join('')) * 1000000
-        } else if (amount.includes('M')) {
-            amount = Number(amount.split('').filter(letter => letter !== 'M').join('')) * 1000000
-        }
+        } 
 
         amount = Number(amount)
         n = Number(n)
-        let avg = amount / n
 
+        let avg = (amount * 0.9) / n
         let tankSplit = Math.round(avg + (amount * 0.1))
-        let normalSplit = Math.round((amount - tankSplit) / (n - 1))
+        let normalSplit = Math.round(avg)
 
-        tankSplit = numberWithCommas(tankSplit)
-        normalSplit = numberWithCommas(normalSplit)
-        
-        message.channel.send(`Tank Split: ${tankSplit}\nNormal Split: ${normalSplit}`)
+        message.channel.send(`Tank Split: ${numberWithCommas(tankSplit)}\nNormal Split: ${numberWithCommas(normalSplit)}`)
     }
 })
 
